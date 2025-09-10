d56685cea6e9dd31f0baeddbd9fb2b1ad262c07c
Stepan <inin_01@mail.ru>
2025-09-11 03:32:20 +0700

    DS-B-4: * файлы миграций

---
+++ DirectoryService/src/DirectoryService.Infrastructure/Migrations/20250910202230_Initial.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "DirectoryService");
+
+            migrationBuilder.CreateTable(
+                name: "department",
+                schema: "DirectoryService",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    identifier = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    parent_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    path = table.Column<string>(type: "character varying(9999)", maxLength: 9999, nullable: false),
+                    depth = table.Column<int>(type: "integer", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_department", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_department_department_parent_id",
+                        column: x => x.parent_id,
+                        principalSchema: "DirectoryService",
+                        principalTable: "department",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "location",
+                schema: "DirectoryService",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    timezone = table.Column<string>(type: "text", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    house_number = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    zip_code = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_location", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "positions",
+                schema: "DirectoryService",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_positions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "department_locations",
+                schema: "DirectoryService",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    location_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    department_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_department_locations", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_department_locations_department_department_id",
+                        column: x => x.department_id,
+                        principalSchema: "DirectoryService",
+                        principalTable: "department",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_department_locations_location_location_id",
+                        column: x => x.location_id,
+                        principalSchema: "DirectoryService",
+                        principalTable: "location",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "department_positions",
+                schema: "DirectoryService",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    department_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    position_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_department_positions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_department_positions_department_department_id",
+                        column: x => x.department_id,
+                        principalSchema: "DirectoryService",
+                        principalTable: "department",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_department_positions_positions_position_id",
+                        column: x => x.position_id,
+                        principalSchema: "DirectoryService",
+                        principalTable: "positions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_department_parent_id",
+                schema: "DirectoryService",
+                table: "department",
+                column: "parent_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_department_locations_department_id",
+                schema: "DirectoryService",
+                table: "department_locations",
+                column: "department_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_department_locations_location_id",
+                schema: "DirectoryService",
+                table: "department_locations",
+                column: "location_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_department_positions_department_id",
+                schema: "DirectoryService",
+                table: "department_positions",
+                column: "department_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_department_positions_position_id",
+                schema: "DirectoryService",
+                table: "department_positions",
+                column: "position_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "department_locations",
+                schema: "DirectoryService");
+
+            migrationBuilder.DropTable(
+                name: "department_positions",
+                schema: "DirectoryService");
+
+            migrationBuilder.DropTable(
+                name: "location",
+                schema: "DirectoryService");
+
+            migrationBuilder.DropTable(
+                name: "department",
+                schema: "DirectoryService");
+
+            migrationBuilder.DropTable(
+                name: "positions",
+                schema: "DirectoryService");
+        }
+    }
+}
