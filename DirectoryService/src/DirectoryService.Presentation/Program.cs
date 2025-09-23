7c3ca8d2cdea335962894fa8e69f576bf3a3f63c
Stepan <inin_01@mail.ru>
2025-09-24 02:40:25 +0700

    DS-B-7: * Добавлен seq и минимальное логирование

--- DirectoryService/src/DirectoryService.Presentation/Program.cs
+++ DirectoryService/src/DirectoryService.Presentation/Program.cs
@@ -3,8 +3,22 @@ using DirectoryService.Infrastructure;
 using DirectoryService.Presentation.Controllers;
 using DirectoryService.Presentation.Middlewares;
 using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                 ?? throw new ArgumentNullException($"Seq"))
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentUserName()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(DirectoryServiceController).Assembly);
@@ -13,7 +27,13 @@ builder.Services.AddOpenApi();
 
 builder.Services
     .AddInfrastructure(builder.Configuration)
-    .AddSpeciesApplication();
+    .AddSpeciesApplication()
+    .AddSerilog();
+
+builder.Services.AddHttpLogging(o =>
+{
+    o.CombineLogs = true;
+});
 
 var app = builder.Build();
 
