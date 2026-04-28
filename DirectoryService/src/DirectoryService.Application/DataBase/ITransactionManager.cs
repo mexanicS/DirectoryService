5c5f88f5088b768b82897abaef4d63ae3a25fd82
Stepan <inin_01@mail.ru>
2026-04-29 00:06:57 +0700

    * добавил TransactionManager и TransactionScope.cs

---
+++ DirectoryService/src/DirectoryService.Application/DataBase/ITransactionManager.cs
@@ -0,0 +1,11 @@
+using CSharpFunctionalExtensions;
+using SharedKernel;
+
+namespace DirectoryService.Application.DataBase;
+
+public interface ITransactionManager
+{
+    Task<Result<ITransactionScope, Error>> BeginTransactionAsync(CancellationToken cancellationToken);
+
+    Task<UnitResult<Error>> SaveChangesAsync(CancellationToken cancellationToken);
+}
